4976411c93ca71223ea870b7f3fe648cfc0eb335
danishm <danish.mujeeb@gmail.com>
2013-06-24 16:27:49 -0400

    Added ability to execute from command prompt

--- README.md
+++ README.md
@@ -1,5 +1,44 @@
 easymagick.py
 =============
 
-A module to simplifying scripting some batch processing of my photographs
-using ImageMagick. 
+A simpler python script to help with batch operations on images using the ImageMagick library. The library has a built in command `mogrify`, but its use is discouraged. This command was designed to edit image in-place and hence runs the risk of accidental loss of your source images if the command is not executed with the right flags.
+
+They recommend using an `sh` shell loop to call the `convert` command. However, I wanted to make it OS independent since I use Windows, Mac OS X and Linux. I also wanted to have no additional dependencies other than the ImageMagick binaries and the standard Python library.
+
+Usage
+-----
+
+The command requires a recipe name and the folder to apply the recipe to
+
+	usage: easymagick.py [-h] recipe folder
+
+	Batch processor for photographs.
+
+	positional arguments:
+	  recipe      Batch processing recepies to use
+	  folder      Full path to folder to apply to
+
+	optional arguments:
+	  -h, --help  show this help message and exit
+
+Example
+-------
+
+To resize images in a folder e.g. `M:\Pictures\Vacation\` for on-screen viewing and place them in a sub folder called "screen" e.g. `M:\Pictures\Vacation\screen\`, execute the following command.
+
+	python easymagick.py screen M:\Pictures\Vacation\
+
+Recipes
+-------
+
+Recipes are simple named templates to define the most typical batch processing actions that you need to do. The recipes are stores in the `RECIPES` variable in the `easymagick.py` script.
+
+### Recipe: screen
+My initial motivation for writing this script was to be able to reduce the size of my Digital SLR images from 24 Mega pixels to 5 Mega pixels i.e. adequate enough to view on a high resolution retina display without loss of quality. I wanted to store these re-sized images in sub-folder of each of my main picture folders.
+
+Future Enhancements
+-------------------
+
+ * Ability to apply a batch operation to a whole folder hierarchy recursively.
+ * Add more recipes.
+ * Add ability to specify a batch operation manually from command line.
--- easymagick.py
+++ easymagick.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import argparse
 import subprocess
 
 PICTURE_FORMATS = ['JPG']
@@ -10,7 +11,12 @@ def path_to_convert():
 	better way of doing this.
 	"""
 	if sys.platform == 'darwin':
+		# For Mac, programs on path are not available, hence have to give explicit paths
 		return '/opt/ImageMagick/bin/convert'
+	elif sys.platform == 'win32':
+		# For windows, executable on the path seems to work just like thats
+		return 'convert'
+
 
 def resize(src, dest, spec):
 	"""
@@ -20,6 +26,7 @@ def resize(src, dest, spec):
 	print ' '.join(cmd)
 	retcode = subprocess.call(' '.join(cmd), shell=True)
 	print retcode
+
 
 def get_files_in_folder(folder):
 	"""
@@ -34,27 +41,51 @@ def get_files_in_folder(folder):
 				files.append(file)
 	return files
 
-def process_folder(folder, outdir, operation, spec):
 
+def apply_batch_operation(folder_in, folder_out, operation, spec):
+	"""
+	Applies an operation as a batch to all files in a folder
+	and stores the result in another folder
+	"""
 	# Checking if destination folder exists
-	if os.path.exists(outdir)==False:
-		print 'Creating folder %s' % outdir
-		os.makedirs(outdir)
+	if os.path.exists(folder_out)==False:
+		print 'Creating folder %s' % folder_out
+		os.makedirs(folder_out)
 
 	# Processing the files
-	for file in get_files_in_folder(folder):
-		src = os.path.join(folder, file)
-		dest = os.path.join(outdir, file)
+	for file in get_files_in_folder(folder_in):
+		src = os.path.join(folder_in, file)
+		dest = os.path.join(folder_out, file)
 		print 'Processing %s -> %s' % (src, dest)
 		operation(src, dest, spec)
 
 
+def process_folder(folder_in, subdir, operation, spec):
+	"""
+	Applies an operation as a batch to all files in a folder 
+	and stores the result in a subdirectory in that folder
+	"""
+	folder_out = os.path.join(folder_in, subdir)
+	apply_batch_operation(folder_in, folder_out, operation, spec)
+
+
+RECIPES = {
+	'screen': {
+		'operation': resize, 'spec': '5000000@', 'subdir': 'screen'
+	}
+}
+
+
 if __name__=='__main__':
 	
-	print path_to_convert()
-
-	process_folder('/Users/danish/Pictures/Picasa Exports/500px',
-		'/Users/danish/Pictures/Picasa Exports/500px/lowres/',
-		resize, '10000@')
+	parser = argparse.ArgumentParser(description='Batch processor for photographs.')
+	parser.add_argument('recipe', help='Batch processing recepies to use')
+	parser.add_argument('folder', help='Full path to folder to apply to')
 	
-	#print get_files_in_folder(r'C:\Users\Danish\Pictures')
+	args = parser.parse_args()
+
+	if args.recipe in RECIPES:
+		recipe_spec = RECIPES[args.recipe]
+		process_folder(args.folder, recipe_spec['subdir'], recipe_spec['operation'], recipe_spec['spec'])
+	else:
+		print 'ERROR: Unknown recipe "%s"' % args.recipe
