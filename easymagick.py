518860ccbddd2ecf0c9b0961aecd9387846c3e95
Danish Mujeeb <danish.mujeeb@gmail.com>
2013-06-30 01:39:21 -0400

    Fixed implementation of overwrite logic

--- easymagick.py
+++ easymagick.py
@@ -23,12 +23,12 @@ def resize(src, dest, spec):
 	Wrapper for resising a picture
 	"""
 	cmd = [path_to_convert(), '"%s"'%src, '-resize %s'%spec, '"%s"'%dest]
-	print ' '.join(cmd)
+	#print ' '.join(cmd)
 	retcode = subprocess.call(' '.join(cmd), shell=True)
-	print retcode
+	#print retcode
 
 
-def get_files_in_folder(folder, skip_existing=True):
+def get_files_in_folder(folder):
 	"""
 	Get a list of filenames in a folder
 	"""
@@ -38,10 +38,7 @@ def get_files_in_folder(folder, skip_existing=True):
 		ext = file[-3:].upper()
 		if os.path.isfile(fullpath):
 			if ext in PICTURE_FORMATS:
-				if os.path.exists(fullpath)==False or skip_existing==False:
-					files.append(file)
-				else:
-					print 'Skipping: ', fullpath
+				files.append(file)
 	return files
 
 
@@ -56,20 +53,25 @@ def apply_batch_operation(folder_in, folder_out, operation, spec, skip_existing=
 		os.makedirs(folder_out)
 
 	# Processing the files
-	for file in get_files_in_folder(folder_in, skip_existing):
+	for file in get_files_in_folder(folder_in):
 		src = os.path.join(folder_in, file)
 		dest = os.path.join(folder_out, file)
-		print 'Processing %s -> %s' % (src, dest)
-		operation(src, dest, spec)
+
+		if os.path.exists(dest)==False or skip_existing==False:
+			print 'Processing %s -> %s' % (src, dest)
+			operation(src, dest, spec)
+		else:
+			print 'Skipping: ', src
+		
 
 
-def process_folder(folder_in, subdir, operation, spec):
+def process_folder(folder_in, subdir, operation, spec, skip_existing=True):
 	"""
 	Applies an operation as a batch to all files in a folder 
 	and stores the result in a subdirectory in that folder
 	"""
 	folder_out = os.path.join(folder_in, subdir)
-	apply_batch_operation(folder_in, folder_out, operation, spec)
+	apply_batch_operation(folder_in, folder_out, operation, spec, skip_existing)
 
 
 RECIPES = {
@@ -84,11 +86,12 @@ if __name__=='__main__':
 	parser = argparse.ArgumentParser(description='Batch processor for photographs.')
 	parser.add_argument('recipe', help='Batch processing recepies to use')
 	parser.add_argument('folder', help='Full path to folder to apply to')
+	parser.add_argument('-o', '--overwrite', action='store_true', default=False, help='Overwrite existing files')
 	
 	args = parser.parse_args()
 
 	if args.recipe in RECIPES:
 		recipe_spec = RECIPES[args.recipe]
-		process_folder(args.folder, recipe_spec['subdir'], recipe_spec['operation'], recipe_spec['spec'])
+		process_folder(args.folder, recipe_spec['subdir'], recipe_spec['operation'], recipe_spec['spec'], not args.overwrite)
 	else:
 		print 'ERROR: Unknown recipe "%s"' % args.recipe
